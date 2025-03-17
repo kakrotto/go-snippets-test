0183e7f78dba24c1d508aac6939bb0cfdf1d87aa
kakrotto <jiezhao458@gmail.com>
2025-03-17 15:33:08 +0800

    update

--- snippets/tgbot.go
+++ snippets/tgbot.go
@@ -48,3 +48,59 @@ func RunTelegramBot() {
 	}
 	fmt.Println("消息已发送！")
 }
+
+func QTelegramBot() {
+	_ = config.InitConfig()
+	token := config.GetConfig().TgBot.Token
+	proxy := config.GetConfig().Proxy.Url
+
+	proxyURL, err := url.Parse(proxy) // 替换为你的代理地址和端口
+	if err != nil {
+		log.Fatal("代理地址解析失败:", err)
+	}
+
+	// 创建一个 HTTP 客户端，带有代理配置
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+		},
+	}
+
+	// 使用你的 API Token 替换
+	bot, err := tgbotapi.NewBotAPIWithClient(token, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot.Debug = true // 开启 Debug 模式，观察日志
+
+	// 获取 Bot 更新
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60 // 设置超时时间
+	updates, _ := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		// 处理消息
+		if update.Message != nil {
+			msgText := update.Message.Text   // 用户输入的文本
+			chatID := update.Message.Chat.ID // 聊天 ID
+
+			var replyText string
+
+			// 简单的自动回复逻辑
+			switch msgText {
+			case "你好":
+				replyText = "你好，有什么可以帮助你的？😊"
+			case "帮助":
+				replyText = "这是一个 FAQ Bot，你可以问我一些常见问题！📌"
+			case "你是谁":
+				replyText = "我是你的 Telegram 机器人 🤖"
+			default:
+				replyText = "抱歉，我不明白你的意思。可以输入 '帮助' 查看支持的指令。"
+			}
+
+			// 发送消息
+			msg := tgbotapi.NewMessage(chatID, replyText)
+			bot.Send(msg)
+		}
+	}
+}
